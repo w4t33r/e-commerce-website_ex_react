daead643c3e748e345e510aa94cb365e10d71017
w44t3r <vk.educate@protonmail.ch>
2022-12-28 17:36:10 +0100

    init project commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # exam_react
 react-frontend_kurs_3
+npm add react-router-dom
---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<span>Home</span>
+  },
+  {
+    path:"products/:id",
+    element:<span>Category</span>
+  },
+  {
+    path:"/product/:id",
+    element:<span>Product</span>
+  },
+])
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  );
+}
+
+export default App;
