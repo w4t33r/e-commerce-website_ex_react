d9f3bef2853eec72d069deb795824aace73f5913
w44t3r <vk.educate@protonmail.ch>
2022-12-28 18:12:09 +0100

    updated components

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+const Navbar = () => {
+    return (
+
+        <div className='navbar'>
+            <div className="wrapper">
+                <div className="left"></div>
+                <div className="center"></div>
+                <div className="right"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
