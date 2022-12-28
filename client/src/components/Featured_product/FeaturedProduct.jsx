cbaa6fff0653cf265b1e4a6b52a87d8e72324569
w44t3r <vk.educate@protonmail.ch>
2022-12-28 23:55:31 +0100

    path fixes

--- client/src/components/Featured_product/FeaturedProduct.jsx
+++ client/src/components/Featured_product/FeaturedProduct.jsx
@@ -17,7 +17,7 @@ const FeaturedProduct = ({type}) => {
             img: "https://cdn.pixabay.com/photo/2021/12/21/03/56/street-6884534_960_720.jpg",
             img2: "https://cdn.pixabay.com/photo/2021/12/05/12/29/christmas-tree-6847584_960_720.jpg",
             title: 'Love',
-            isNew: true,
+            isNew: false,
             oldPrice: 200,
             price: 100,
         },
--- client/src/page/Home/Home.jsx
+++ client/src/page/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Slider from '../../components/Slider/Slider'
 import './home.scss'
+import FeaturedProduct from "../../components/Featured_product/FeaturedProduct";
 const Home = () => {
     return (
         <div className='home'>
         <Slider/>
+            <FeaturedProduct type='Featured'/>
+            <FeaturedProduct type='Trend'/>
         </div>
     );
 };
