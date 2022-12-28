337e2170c52f73990a26281bf30b9d818ed1cea3
w44t3r <vk.educate@protonmail.ch>
2022-12-28 19:04:38 +0100

    markup & styles navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,68 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+import './Navbar.scss'
+
+
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+
 const Navbar = () => {
     return (
 
         <div className='navbar'>
             <div className="wrapper">
-                <div className="left"></div>
-                <div className="center"></div>
-                <div className="right"></div>
+                <div className="left">
+                    <div className="item">
+                        <img src="/img/sv.png" alt="valute" className=""/>
+                        <KeyboardArrowDownIcon/>
+                    </div>
+                    <div className="item">
+                        <span>SV</span>
+                        <KeyboardArrowDownIcon/>
+                    </div>
+
+                    <div className="item">
+                        <Link className='link' to='/products/1'>Women</Link>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to='/products/2'>Men</Link>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to='/products/3'>Children</Link>
+                    </div>
+                </div>
+
+                <div className="center">
+                    <Link className='link' to='/'>TANYASTORE</Link>
+                </div>
+
+                <div className="right">
+                    <div className="item">
+                        <Link className='link' to='/'>About</Link>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to='/'>Contact</Link>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to='/'>Stores</Link>
+                    </div>
+                    <div className="icons">
+                        <SearchIcon/>
+                        <AccountCircleIcon/>
+                        <FavoriteBorderIcon/>
+                        <div className="cart__icon">
+                            <ShoppingCartIcon/>
+                            <span>0</span>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
