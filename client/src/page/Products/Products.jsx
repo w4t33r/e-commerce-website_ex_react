5c3b3fa5077a001ebf4aaa6cf669d34184423613
w44t3r <vk.educate@protonmail.ch>
2022-12-29 05:12:06 +0100

    products markup, func & styles

--- client/src/page/Products/Products.jsx
+++ client/src/page/Products/Products.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
-import './products.scss'
+import React, {useState} from 'react';
+import {useParams} from 'react-router-dom';
+import './products.scss';
+import ProductList from "../../components/Product_List/ProductList";
 
 const Products = () => {
+
+    const categoryId = parseInt(useParams().id)
+    const [maxPrice, setMaxPrice] = useState(80000)
+    const [sort, setSort] = useState(null)
     return (
         <div className='products'>
             <div className='left'>
@@ -28,24 +34,25 @@ const Products = () => {
                     <h2>Sort by price</h2>
                     <div className="inputItem">
                         <span>0</span>
-                        <input type='range' min={0} max={20000}/>
-                        <span>80000</span>
+                        <input type='range' min={0} max={80000} onChange={(e) => setMaxPrice(e.target.value)}/>
+                        <span>{maxPrice}</span>
                     </div>
                 </div>
                 <div className="filterItem">
                     <h2>Sort by</h2>
                     <div className="inputItem">
-                        <input type="radio" id='asc' value='asc' name='price'/>
+                        <input type="radio" id='asc' value='asc' name='price' onChange={e=>setSort('asc')}/>
                         <label htmlFor="asc"> Price (Lowest first)</label>
                     </div>
                     <div className="inputItem">
-                        <input type="radio" id='asc' value='asc' name='price'/>
+                        <input type="radio" id='asc' value='asc' name='price' onChange={e=>setSort('desc')}/>
                         <label htmlFor="asc"> Price (Highest first)</label>
                     </div>
                 </div>
             </div>
             <div className="right">
-                <img className='products__img' src="" alt=""/>
+                <img className='products__img' src="https://cdn.pixabay.com/photo/2017/05/05/16/07/paper-boat-2287575_960_720.jpg" alt=""/>
+                <ProductList categoryId={categoryId} maxPrice={maxPrice} sort={sort}/>
             </div>
         </div>
     );
