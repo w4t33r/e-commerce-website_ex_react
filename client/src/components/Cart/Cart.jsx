bee00f5c8a44229faccd1c7f687d591a89118031
w44t3r <vk.educate@protonmail.ch>
2022-12-29 08:47:03 +0100

    cart markup and func

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import './cart.scss'
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 const Cart = () => {
     const data = [
         {
@@ -7,6 +8,7 @@ const Cart = () => {
             img: "https://cdn.pixabay.com/photo/2021/12/21/03/56/street-6884534_960_720.jpg",
             img2: "https://cdn.pixabay.com/photo/2021/12/05/12/29/christmas-tree-6847584_960_720.jpg",
             title: 'Love',
+            desc: 'Love',
             isNew: true,
             oldPrice: 200,
             price: 100,
@@ -16,6 +18,7 @@ const Cart = () => {
             img: "https://cdn.pixabay.com/photo/2021/12/21/03/56/street-6884534_960_720.jpg",
             img2: "https://cdn.pixabay.com/photo/2021/12/05/12/29/christmas-tree-6847584_960_720.jpg",
             title: 'Love',
+            desc: 'Love',
             isNew: false,
             oldPrice: 200,
             price: 100,
@@ -24,6 +27,23 @@ const Cart = () => {
     return (
         <div className='cart'>
             <h2>Products in your cart</h2>
+            {data?.map(item => (
+                <div className='cartItem'> key={item.id}
+                    <img src={item.img} alt=""/>
+                    <div className="cartDetails">
+                        <h2>{item.title}</h2>
+                        <p>{item.desc?.substring(0, 100)}</p>
+                        <div className="cartPrice"> 1 x {item.price}</div>
+                    </div>
+                    <DeleteOutlinedIcon className='cartDelete'/>
+                </div>
+            ))}
+            <div className="cartTotal">
+                <span>SUBTOTAL</span>
+                <span>$132</span>
+            </div>
+            <button>PROCEED TO CHECKOUT</button>
+            <span className='cartReset'></span>
         </div>
     );
 };
