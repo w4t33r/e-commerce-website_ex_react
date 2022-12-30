6a50ec7f1ec01e74636152d1dab244bff5756cf2
w44t3r <vk.educate@protonmail.ch>
2022-12-30 03:21:09 +0100

    bug resolved, hook at the top level

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,31 +1,27 @@
-import {useEffect, useState} from "react";
-import {makeRequest} from "../makeRequest";
 
-const useFetch = async (url) => {
-    const [data, setData] = useState(null);
+import { useEffect, useState } from "react";
+import { makeRequest } from "../makeRequest";
+
+const useFetch = (url) => {
+    const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                setLoading(true)
-                const res = await makeRequest.get(url,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
-                        },
-                    }
-                );
-                console.log(res)
-                setData(res.data.data)
+                setLoading(true);
+                const res = await makeRequest.get(url);
+                setData(res.data.data);
             } catch (err) {
-                setError(true)
+                setError(true);
             }
-            setLoading(false)
+            setLoading(false);
         };
-        fetchData()
+        fetchData();
     }, [url]);
-  return {data, loading, error}
+
+    return { data, loading, error };
 };
-export default useFetch()
+
+export default useFetch;
