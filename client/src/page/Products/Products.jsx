2c6aec5f977a137b1b8e1d5d49cbf023c95ecefa
w44t3r <vk.educate@protonmail.ch>
2022-12-30 06:19:16 +0100

    subCategory filters

--- client/src/page/Products/Products.jsx
+++ client/src/page/Products/Products.jsx
@@ -2,33 +2,40 @@ import React, {useState} from 'react';
 import {useParams} from 'react-router-dom';
 import './products.scss';
 import ProductList from "../../components/Product_List/ProductList";
+import useFetch from "../../hooks/useFetch";
 
 const Products = () => {
 
     const categoryId = parseInt(useParams().id)
     const [maxPrice, setMaxPrice] = useState(80000)
     const [sort, setSort] = useState(null)
+    const [selectedSubCategory, setSelectedSubCategory] = useState([])
+
+    const {data, loading, error} = useFetch(`/sub-categories?[filters][categories][id][$eq]=${categoryId}`)
+
+    const handleChange = (e) => {
+        const value = e.target.value
+        const isChecked = e.target.checked
+
+        setSelectedSubCategory(
+            isChecked
+                ?[...selectedSubCategory, value]
+                :selectedSubCategory.filter((item)=> item !== value)
+        );
+    };
+    console.log(selectedSubCategory)
+
     return (
         <div className='products'>
             <div className='left'>
                 <div className="filterItem">
                     <h2>Product Categories</h2>
-                    <div className="inputItem">
-                        <input type='checkbox' id='1' value={1}/>
-                        <label htmlFor='1'>Shoes</label>
-                    </div>
-                    <div className="inputItem">
-                        <input type='checkbox' id='2' value={2}/>
-                        <label htmlFor='2'>Accesory</label>
-                    </div>
-                    <div className="inputItem">
-                        <input type='checkbox' id='3' value={3}/>
-                        <label htmlFor='3'>Weeding dress</label>
-                    </div>
-                    <div className="inputItem">
-                        <input type='checkbox' id='4' value={4}/>
-                        <label htmlFor='4'>Night dress</label>
-                    </div>
+                    {data?.map((item) => (
+                        <div className="inputItem" key={item.id}>
+                            <input type='checkbox' id={item.id} value={item.id} onChange={handleChange}/>
+                            <label htmlFor={item.id}>{item.attributes.title}</label>
+                        </div>
+                    ))}
                 </div>
                 <div className="filterItem">
                     <h2>Sort by price</h2>
@@ -41,18 +48,19 @@ const Products = () => {
                 <div className="filterItem">
                     <h2>Sort by</h2>
                     <div className="inputItem">
-                        <input type="radio" id='asc' value='asc' name='price' onChange={e=>setSort('asc')}/>
+                        <input type="radio" id='asc' value='asc' name='price' onChange={e => setSort('asc')}/>
                         <label htmlFor="asc"> Price (Lowest first)</label>
                     </div>
                     <div className="inputItem">
-                        <input type="radio" id='asc' value='asc' name='price' onChange={e=>setSort('desc')}/>
+                        <input type="radio" id='asc' value='asc' name='price' onChange={e => setSort('desc')}/>
                         <label htmlFor="asc"> Price (Highest first)</label>
                     </div>
                 </div>
             </div>
             <div className="right">
-                <img className='products__img' src="https://cdn.pixabay.com/photo/2017/05/05/16/07/paper-boat-2287575_960_720.jpg" alt=""/>
-                <ProductList categoryId={categoryId} maxPrice={maxPrice} sort={sort}/>
+                <img className='products__img'
+                     src="https://cdn.pixabay.com/photo/2017/05/05/16/07/paper-boat-2287575_960_720.jpg" alt=""/>
+                <ProductList categoryId={categoryId} maxPrice={maxPrice} sort={sort} subCat = {selectedSubCategory}/>
             </div>
         </div>
     );
