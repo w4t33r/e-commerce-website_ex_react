19fb18322ff9b07d88d550daf913ce3439865c32
w44t3r <vk.educate@protonmail.ch>
2023-01-01 18:03:56 +0100

    added query request & query filters

--- client/src/components/Product_List/ProductList.jsx
+++ client/src/components/Product_List/ProductList.jsx
@@ -1,69 +1,21 @@
 import React from 'react';
 import './product_list.scss'
 import Card from "../Cards/Card";
+import useFetch from "../../hooks/useFetch";
 
-const ProductList = () => {
-    const data = [
-        {
-            id: 1,
-            img: "https://cdn.pixabay.com/photo/2021/12/21/03/56/street-6884534_960_720.jpg",
-            img2: "https://cdn.pixabay.com/photo/2021/12/05/12/29/christmas-tree-6847584_960_720.jpg",
-            title: 'Love',
-            isNew: true,
-            oldPrice: 700,
-            price: 400,
-        },
-        {
-            id: 2,
-            img: "https://cdn.pixabay.com/photo/2021/12/21/03/56/street-6884534_960_720.jpg",
-            img2: "https://cdn.pixabay.com/photo/2021/12/05/12/29/christmas-tree-6847584_960_720.jpg",
-            title: 'Love',
-            isNew: false,
-            oldPrice: 6000,
-            price: 3000,
-        },
-        {
-            id: 3,
-            img: "https://cdn.pixabay.com/photo/2021/12/21/03/56/street-6884534_960_720.jpg",
-            img2: "https://cdn.pixabay.com/photo/2021/12/05/12/29/christmas-tree-6847584_960_720.jpg",
-            title: 'Love',
-            isNew: true,
-            oldPrice: 200,
-            price: 11,
-        },
-        {
-            id: 4,
-            img: "https://cdn.pixabay.com/photo/2021/12/21/03/56/street-6884534_960_720.jpg",
-            img2: "https://cdn.pixabay.com/photo/2021/12/05/12/29/christmas-tree-6847584_960_720.jpg",
-            title: 'Love',
-            isNew: true,
-            oldPrice: 700,
-            price: 400,
-        },
-        {
-            id: 5,
-            img: "https://cdn.pixabay.com/photo/2021/12/21/03/56/street-6884534_960_720.jpg",
-            img2: "https://cdn.pixabay.com/photo/2021/12/05/12/29/christmas-tree-6847584_960_720.jpg",
-            title: 'Love',
-            isNew: false,
-            oldPrice: 6000,
-            price: 3000,
-        },
-        {
-            id: 6,
-            img: "https://cdn.pixabay.com/photo/2021/12/21/03/56/street-6884534_960_720.jpg",
-            img2: "https://cdn.pixabay.com/photo/2021/12/05/12/29/christmas-tree-6847584_960_720.jpg",
-            title: 'Love',
-            isNew: true,
-            oldPrice: 200,
-            price: 11,
-        },
-    ]
+const ProductList = ({subCat, maxPrice, sort, categoryId}) => {
+
+    const {data, loading, error} = useFetch(
+        `/products?populate=*&[filters][categories][id]=${categoryId}${subCat.map(
+            item => `&[filters][sub_categories][id][$eq]=${item}`
+        )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`)
+
+
     return (
-        <div className='product__list'>{data.map(item=>(
-            <Card item={item} key={item.id}/>
-        )
-       )}
+        <div className='product__list'>
+            {loading
+                ? "loading"
+                : data?.map((item) => <Card item={item} key={item.id}/>)}
         </div>
     );
 };
