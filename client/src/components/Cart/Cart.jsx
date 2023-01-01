f9803266739bb5e7a3a1196ee3296ce3bbd13679
w44t3r <vk.educate@protonmail.ch>
2023-01-01 17:58:59 +0100

    reworked car --> redux state & dispatch

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,49 +1,38 @@
 import React from 'react';
 import './cart.scss'
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import {useDispatch, useSelector} from "react-redux";
+import {removeItem, resetCart} from "../../redux/cartReducer";
+
 const Cart = () => {
-    const data = [
-        {
-            id: 1,
-            img: "https://cdn.pixabay.com/photo/2021/12/21/03/56/street-6884534_960_720.jpg",
-            img2: "https://cdn.pixabay.com/photo/2021/12/05/12/29/christmas-tree-6847584_960_720.jpg",
-            title: 'Weeeding Dress NEw',
-            desc: 'Love',
-            isNew: true,
-            oldPrice: 200,
-            price: 100,
-        },
-        {
-            id: 2,
-            img: "https://cdn.pixabay.com/photo/2021/12/21/03/56/street-6884534_960_720.jpg",
-            img2: "https://cdn.pixabay.com/photo/2021/12/05/12/29/christmas-tree-6847584_960_720.jpg",
-            title: 'Love',
-            desc: 'Love',
-            isNew: false,
-            oldPrice: 200,
-            price: 100,
-        },
-    ]
+    const products = useSelector(state => state.cart.products)
+    const dispatch = useDispatch();
+    const totalPrice = () =>{
+        let total = 0
+        products.forEach((item) => (total += item.quantity * item.price))
+        return total.toFixed(2)
+    }
     return (
         <div className='cart'>
             <h2>Products in your cart</h2>
-            {data?.map(item => (
+            {products?.map(item => (
                 <div className='cartItem' key={item.id}>
-                    <img src={item.img} alt=""/>
+                    <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt=""/>
                     <div className="cartDetails">
                         <h2>{item.title}</h2>
                         <p>{item.desc?.substring(0, 100)}</p>
-                        <div className="cartPrice"> 1 x {item.price}</div>
+                        <div className="cartPrice">
+                            {item.quantity} > SEK:{item.price}
+                        </div>
                     </div>
-                    <DeleteOutlinedIcon className='cartDelete'/>
+                    <DeleteOutlinedIcon className='cartDelete' onClick={() => dispatch(removeItem(item.id))}/>
                 </div>
             ))}
             <div className="cartTotal">
-                <span>SUBTOTAL</span>
-                <span>$132</span>
+                <span>${totalPrice()}</span>
             </div>
             <button>PROCEED TO CHECKOUT</button>
-            <span className='cartReset'> Reset Cart</span>
+            <span className='cartReset' onClick={() => dispatch(resetCart())}> Reset Cart </span>
         </div>
     );
 };
