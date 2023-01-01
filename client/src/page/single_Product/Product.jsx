01b86ad849a1fee22fe7aa20f1bb5f26bd167748
w44t3r <vk.educate@protonmail.ch>
2023-01-01 18:06:19 +0100

    Deleted main image, realise addToCart button

--- client/src/page/single_Product/Product.jsx
+++ client/src/page/single_Product/Product.jsx
@@ -1,69 +1,76 @@
 import React from 'react';
 import './product.scss'
 import {useState} from 'react'
+import {useParams} from "react-router-dom";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
+import useFetch from "../../hooks/useFetch";
+import {useDispatch} from "react-redux";
+import {addToCart} from "../../redux/cartReducer";
 
 const Product = () => {
-    const images = [
-        "https://cdn.pixabay.com/photo/2016/06/29/08/41/wedding-dresses-1486256_960_720.jpg",
-        "https://cdn.pixabay.com/photo/2021/07/14/15/43/woman-6466382_960_720.jpg",
-    ];
-    const [selectedImg, setSelectedImg] = useState(0)
+    const id = useParams().id;
+    const [selectedImg, setSelectedImg] = useState("img");
     const [quantity, setQuantity] = useState(1)
+
+    const dispatch = useDispatch();
+    const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
     return (
         <div className='product'>
-            <div className="imageContainer">
-                <div className="images">
-                    <img src={images[0]} alt="" onClick={() => setSelectedImg(0)}/>
-                    <img src={images[1]} alt="" onClick={() => setSelectedImg(1)}/>
-                </div>
-                <div className="mainImg">
-                    <img src={images[selectedImg]} alt=""/>
-                </div>
-            </div>
-            <div className="productInfo">
-                <h2>title</h2>
-                <span className='price'>222 sek</span>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cupiditate dignissimos iusto odio
-                    praesentium quisquam. Aspernatur assumenda beatae eos exercitationem ipsa iure, libero magni, quis
-                    rem rerum sed vero. Praesentium?
-                </p>
-                <div className="quantity">
-                    <button onClick={() => {
-                        setQuantity(prev => prev === 1 ? 1 : prev - 1)
-                    }}>-
-                    </button>
-                    {quantity}
-                    <button onClick={() => {
-                        setQuantity(prev => prev + 1)
-                    }}>+
-                    </button>
-                </div>
-                <button className="addToCart">
-                    <AddShoppingCartIcon/> ADD TO CART
-                </button>
-                <div className="link">
-                    <div className="item">
-                        <FavoriteBorderIcon/> ADD TO WISHLIST
-                    </div>
-                    <div className="item">
-                        <BalanceIcon/>ADD TO COMPARE
+            {loading ? "loading" : (<>
+                <div className="imageContainer">
+                    <div className="mainImg">
+                        <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img?.data?.attributes?.url} alt="" onClick={() => setSelectedImg("img")}/>
+                        <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img2?.data?.attributes?.url} alt="" onClick={() => setSelectedImg("img2")}/>
                     </div>
                 </div>
-                <div className="itemInfo">
-                    <span>Lorem: Lorem</span>
-                    <span>Product Type: Lorem</span>
-                    <span>Tag: Dress, Weeding</span>
-                </div>
-                <hr />
-                <div className="itemInfo">
-                    <span>DESCRIPTION</span>
-                    <span>ADDITIONAL INFORMATION</span>
-                    <span>FAQ</span>
-                </div>
-            </div>
+                <div className="productInfo">
+                    <h2>{data?.attributes?.title}</h2>
+                    <span className='price'>${data?.attributes?.price}</span>
+                    <p>{data?.attributes?.desc}
+                    </p>
+                    <div className="quantity">
+                        <button onClick={() => {
+                            setQuantity(prev => prev === 1 ? 1 : prev - 1)
+                        }}>-
+                        </button>
+                        {quantity}
+                        <button onClick={() => {
+                            setQuantity(prev => prev + 1)
+                        }}>+
+                        </button>
+                    </div>
+                    <button className="addToCart" onClick={()=> dispatch(addToCart({
+                        id:data.id,
+                        title:data.attributes.title,
+                        desc:data.attributes.desc,
+                        price:data.attributes.price,
+                        img:data.attributes.img.data.attributes.url,
+                        quantity,
+                    }))}>
+                        <AddShoppingCartIcon/> ADD TO CART
+                    </button>
+                    <div className="link">
+                        <div className="item">
+                            <FavoriteBorderIcon/> ADD TO WISHLIST
+                        </div>
+                        <div className="item">
+                            <BalanceIcon/>ADD TO COMPARE
+                        </div>
+                    </div>
+                    <div className="itemInfo">
+                        <span>Lorem: Lorem</span>
+                        <span>Product Type: Lorem</span>
+                        <span>Tag: Dress, Weeding</span>
+                    </div>
+                    <hr/>
+                    <div className="itemInfo">
+                        <span>DESCRIPTION</span>
+                        <span>ADDITIONAL INFORMATION</span>
+                        <span>FAQ</span>
+                    </div>
+                </div></> )}
         </div>
     );
 };
